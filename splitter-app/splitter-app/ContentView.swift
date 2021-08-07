9ba1e3c1bb3e72f92989cba0eb1c3d937e5543ff
Balguzhinov Abay <75177898+balguzhinov@users.noreply.github.com>
2021-08-07 17:16:54 +0600

    Add files via upload

---
+++ splitter-app/splitter-app/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  splitter-app
+//
+//  Created by selimmh on 04.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [0,10,15,20,30]
+    
+    var totalPerPerson: Int {
+        let peopleCount = Int(numberOfPeople + 2)
+        let tipSelection = Int(tipPercentages[tipPercentage])
+        let orderAmount = Int(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Сумма", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Количество людей", selection: $numberOfPeople) {
+                        ForEach(2 ..< 15) {
+                            Text("\($0) человек")
+                        }
+                    }
+                }
+                
+                Section (header: Text("Какой процент чаевых вы хотите оставить?")){
+                    
+                    Picker("Процент чаевых", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("Общая сумма выходит: ")){
+                    Text("₸\(totalPerPerson)")
+                }
+            }
+            .navigationBarTitle("Чек-разделитель")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
